e870ae5374943ed2ea4fa17d542c3217689e2f04
Your Nameimbhavaniprasad <bhavaniprasadsmart@gmail.com>
2025-05-03 11:27:56 +0530

    to test updated changes deployment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Hello from Lambda!",
+            message: "Hey Bhav!",
         }),
     };
 
