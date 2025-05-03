bb5c3580c98aa4db86d541a2bbeb959a4194c115
Your Nameimbhavaniprasad <bhavaniprasadsmart@gmail.com>
2025-05-03 10:54:53 +0530

    deploy lambda action

---
+++ index.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+    // Log the event for debugging
+    console.log("Event: ", event);
+
+    // Create a response
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello from Lambda!",
+        }),
+    };
+
+    return response;
+};
